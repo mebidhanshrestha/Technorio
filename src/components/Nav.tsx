f639a4da0b65ca44947e8b3c508b50646dee1131
paleswan1 <pallu.sth1@gmail.com>
2024-08-28 11:19:48 +0545

    h

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import '../index.css'
+
+const Nav = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">Pallu shrestha</h2>
+      <h1 className="bg-yellow">Call ME</h1>
+    </div>
+  )
+}
+
+export default Nav
